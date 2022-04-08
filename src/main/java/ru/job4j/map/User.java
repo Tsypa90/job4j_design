8772f415218e3ac52c2dadbdf165f87cc8134134
Tsypa90 <URSA2004@mail.ru>
2022-04-08 12:36:06 +0300

    3. Переопределить только hashCode [#1003 #286003]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private String name;
@@ -11,5 +11,42 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "name='" + name + '\''
+                + ", children=" + children
+                + ", birthday=" + birthday
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        User one = new User("Pavel", 1, new GregorianCalendar(1990, 9, 03));
+        User two = new User("Pavel", 1, new GregorianCalendar(1990, 9, 03));
+        Map<User, Object> map = new HashMap<>();
+        map.put(one, new Object());
+        map.put(two, new Object());
+        for (User test : map.keySet()) {
+            System.out.println(test.toString() + " " + map.get(test));
+        }
+    }
+/*
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }*/
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
     }
 }
