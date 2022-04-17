fbb296003e2380ab6ed6e37474e1fceb4464773b
Tsypa90 <URSA2004@mail.ru>
2022-04-17 18:16:02 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,21 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            out.write("Результат вычесления:".getBytes());
+            out.write(System.lineSeparator().getBytes());
+            for (int row = 0; row < 9; row++) {
+                for (int cell = 0; cell < 9; cell++) {
+                    out.write(String.valueOf((row + 1) * (cell + 1)).getBytes());
+                    out.write(" ".getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
