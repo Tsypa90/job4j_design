486895945ffe3663cd9a6d197a11b78991023ba5
Tsypa90 <URSA2004@mail.ru>
2022-04-18 22:08:53 +0300

    2. Анализ доступности сервера. [#859 #290556]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -8,23 +8,21 @@ public class Analizy {
 
     public void unavailable(String source, String target) {
         try (BufferedReader log = new BufferedReader(new FileReader(source))) {
-            List<String> downTime = new ArrayList<>();
+            try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
             String serverDownTime = null;
             for (String line = log.readLine(); line != null; line = log.readLine()) {
-                if ((line.contains("400") || line.contains("500"))) {
-                    if (serverDownTime == null) {
-                        serverDownTime = line.substring(line.indexOf(' ') + 1);
-                    }
-                } else {
+                    if ((line.contains("400") || line.contains("500"))) {
+                        if (serverDownTime == null) {
+                            serverDownTime = line.substring(line.indexOf(' ') + 1);
+                        }
+                    } else {
                         serverDownTime = serverDownTime + ";" + line.substring(line.indexOf(' ') + 1);
-                    if (!serverDownTime.substring(0, serverDownTime.indexOf(';')).equals("null")) {
-                        downTime.add(serverDownTime);
+                        if (!serverDownTime.substring(0, serverDownTime.indexOf(';')).equals("null")) {
+                            out.println(serverDownTime);
+                        }
+                        serverDownTime = null;
                     }
-                    serverDownTime = null;
                 }
-            }
-            try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-                downTime.stream().forEach(out::println);
             }
         } catch (IOException e) {
             e.printStackTrace();
