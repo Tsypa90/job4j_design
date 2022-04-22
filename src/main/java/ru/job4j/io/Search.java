b5297d1505367910a3fd3c8ca19f26de965425a7
Tsypa90 <URSA2004@mail.ru>
2022-04-22 17:29:03 +0300

    5. Валидация параметров запуска. [#246865 #292236]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,16 +10,19 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Root folder or condition is null.");
+        }
         validationArgs(args);
         Path start = Paths.get(args[0]);
         search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     private static void validationArgs(String[] args) {
-        if (args.length == 0 || !args[0].contains("/")) {
-            throw new IllegalArgumentException("Root folder is null or not correct. Usage java -jar Search.jar ROOT_FOLDER.");
-        } else if (args.length == 1 || !args[1].contains(".")) {
-            throw new IllegalArgumentException("Condition is null or not correct. Usage java -jar Search.jar CONDITION");
+        if (!new File(args[0]).isDirectory()) {
+            throw new IllegalArgumentException("Root folder is not correct.");
+        } else if (!args[1].contains(".")) {
+            throw new IllegalArgumentException("Condition is not correct.");
         }
     }
 
