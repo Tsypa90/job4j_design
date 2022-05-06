9320b5d028349737a30e64e4a17434b6ac4054a3
Tsypa90 <URSA2004@mail.ru>
2022-05-06 12:31:05 +0300

    1. Inner join [#373617]

--- SQL/item/Inner.sql
+++ SQL/item/Inner.sql
@@ -1,3 +1,29 @@
+create table users_contact(
+	id serial primary  key,
+	city text,
+	phone int
+);
+
+create table users(
+	id serial primary key,
+	name text,
+	users_contact_id int references users_contact(id)
+);
+
+insert into users_contact(city, phone) values('Moscow', 649469);
+insert into users_contact(city, phone) values('Kazan', 252525);
+insert into users_contact(city, phone) values('Perm', 744535);
+insert into users_contact(city, phone) values('Sochi', 324244);
+insert into users_contact(city, phone) values('SPb', 98764534);
+
+insert into users(name, users_contact_id) values('Pavel', 1);
+insert into users(name, users_contact_id) values('Olga', 2);
+insert into users(name, users_contact_id) values('Dima', 5);
+insert into users(name, users_contact_id) values('Gena', 3);
+insert into users(name, users_contact_id) values('Varvara', 4);
+insert into users(name) values('Evgeniy');
+insert into users(name) values('Gosha');
+
 select * from users inner join users_contact uc
 on users.users_contact_id = uc.id;
 
