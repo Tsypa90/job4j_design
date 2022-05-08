22f2e71c29e1facfc522e3bc465a9b202dcb1284
Tsypa90 <URSA2004@mail.ru>
2022-05-08 10:17:41 +0300

    2. Группировка и агрегатные функции [#373618]

---
+++ SQL/item/group.sql
@@ -0,0 +1,33 @@
+insert into devices(name, price) values('Iphone 12', 75000);
+insert into devices(name, price) values('Iphone 13 ProMax', 125000);
+insert into devices(name, price) values('Iphone 8', 15000);
+insert into devices(name, price) values('Samsung S10', 55000);
+insert into devices(name, price) values('Apple Watch', 25000);
+insert into devices(name, price) values('Samsung Watch', 20000);
+insert into devices(name, price) values('Alcatel A2', 3000);
+
+insert into people(name) values ('Pavel');
+insert into people(name) values ('Boris');
+insert into people(name) values ('Olga');
+insert into people(name) values ('Genadiy');
+insert into people(name) values ('Maksim');
+
+insert into devices_people(device_id, people_id) values (3, 1);
+insert into devices_people(device_id, people_id) values (7, 1);
+insert into devices_people(device_id, people_id) values (2, 2);
+insert into devices_people(device_id, people_id) values (5, 2);
+insert into devices_people(device_id, people_id) values (3, 3);
+insert into devices_people(device_id, people_id) values (6, 3);
+insert into devices_people(device_id, people_id) values (5, 4);
+insert into devices_people(device_id, people_id) values (4, 4);
+insert into devices_people(device_id, people_id) values (2, 5);
+insert into devices_people(device_id, people_id) values (3, 5);
+
+select avg(price) from devices;
+
+select s.people_id, avg(ss.price)
+from devices_people as s
+join devices as ss
+on s.device_id = ss.id
+group by s.people_id
+having avg(ss.price) > 50000;
