d8703d8458b2fab7b0c86f56562aeb7d8611900c
Tsypa90 <URSA2004@mail.ru>
2022-05-09 21:20:11 +0300

    0. Join [#6862 #298777]

--- SQL/item/join.sql
+++ SQL/item/join.sql
@@ -33,10 +33,7 @@ select * from departments d cross join employees e;
 select * from departments d left join employees e on e.department_id = d.id where department_id is null;
 
 select * from employees e left join departments d on e.department_id = d.id;
-select * from employees e right join departments d on e.department_id = d.id where department_id is not null;
-
-select * from departments d left join employees e on e.department_id = d.id where department_id is not null;
-select * from departments d right join employees e on e.department_id = d.id;
+select e.id, e.name, e.department_id, d.id, d.name from departments d right join employees e on e.department_id = d.id;
 
 create table teens(
 	name text,
