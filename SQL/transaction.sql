5faf1cde1e097acded61c75f0c99b55da48fc716
Tsypa90 <URSA2004@mail.ru>
2022-08-29 22:17:37 +0300

    4.3. Транзакции. Уровни изолированности транзакций. [#504812]

---
+++ SQL/transaction.sql
@@ -0,0 +1,64 @@
+create table if not exists account (
+    id serial primary key,
+    name varchar,
+    balance float
+);
+
+insert into account (name, balance) VALUES ('Pavel', 1000);
+insert into account (name, balance) VALUES ('Olga', 500);
+insert into account (name, balance) VALUES ('Andrey', 700);
+
+begin transaction;
+update account set balance = balance + 100 where id = 2;
+update account set balance = balance - 100 where id = 1;
+
+begin transaction;
+select * from account;
+
+commit;
+
+select * from account;
+commit;
+
+delete from products;
+ALTER SEQUENCE products_id_seq RESTART WITH 1;
+
+insert into account (name, balance) VALUES ('Pavel', 1000);
+insert into account (name, balance) VALUES ('Olga', 500);
+insert into account (name, balance) VALUES ('Andrey', 700);
+
+begin transaction isolation level repeatable read;
+update account set balance = balance + 100 where id = 2;
+
+begin transaction isolation level repeatable read;
+update account set balance = balance + 100 where id = 2;
+
+commit;
+
+commit;
+
+delete from products;
+ALTER SEQUENCE products_id_seq RESTART WITH 1;
+
+insert into account (name, balance) VALUES ('Pavel', 1000);
+insert into account (name, balance) VALUES ('Olga', 500);
+insert into account (name, balance) VALUES ('Andrey', 700);
+
+begin transaction isolation level serializable;
+
+begin transaction isolation level serializable;
+
+select * from account;
+
+select * from account:
+
+update account set balance = balance + 100 where id = 1;
+
+update account set balance = balance - 1000 where id = 1;
+
+commit;
+
+
+
+
+
