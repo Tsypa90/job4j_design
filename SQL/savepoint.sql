719a3d7a26b1a99ba1b63b6245610a9cf567bb60
Tsypa90 <URSA2004@mail.ru>
2022-08-29 23:03:40 +0300

    4.4. Транзакции. Работа с транзакциями в PostgreSQL [#504813]

---
+++ SQL/savepoint.sql
@@ -0,0 +1,39 @@
+begin transaction;
+select * from account;
+insert into account (name, balance) values('Pavel', 100);
+select * from account;
+savepoint first;
+update account set balance = 0 where id = 1;
+drop table account;
+select * from account;
+rollback to first;
+select * from account;
+commit;
+
+begin transaction;
+select * from account;
+savepoint first;
+update account set balance = 0 where id = 1;
+select * from account;
+savepoint second;
+delete from account;
+select * from account;
+rollback to second;
+select * from account;
+rollback to first;
+select * from account;
+commit;
+
+begin transaction;
+update account set balance = 0 where id = 1;
+savepoint first;
+delete from account;
+savepoint second;
+insert into account (name, balance) values('Olga', 1000);
+select * from account;
+rollback to first;
+select * from account;
+rollback to second;
+commit;
+select * from account;
+
